adf08566a249ce66d3e8aec83d3e7f6f783cdae8
alex.s <shikorin@mail.ru>
2022-11-13 23:36:45 +0300

    description rools

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -32,7 +32,7 @@ class MarvelService {
         return {
             // name: res.data.results[0].name,
             name: char.name,
-            description: char.description,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage:  char.urls[0].url,
             wiki: char.urls[1].url
